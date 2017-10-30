29267a91379e7c5e9f365dcf7a59ecd6aec62ea8
dogukanozturkan <dogukanozturkan@gmail.com>
2017-10-30 17:29:53 +0300

    properties changed for heroku db

--- src/main/java/com/weatherapp/springmvc/dao/UserProfileDaoImpl.java
+++ src/main/java/com/weatherapp/springmvc/dao/UserProfileDaoImpl.java
@@ -9,10 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import com.weatherapp.springmvc.model.UserProfile;
 
-
-
 @Repository("userProfileDao")
-public class UserProfileDaoImpl extends AbstractDao<Integer, UserProfile>implements UserProfileDao{
+public class UserProfileDaoImpl extends AbstractDao<Integer, UserProfile> implements UserProfileDao {
 
 	public UserProfile findById(int id) {
 		return getByKey(id);
@@ -23,12 +21,13 @@ public class UserProfileDaoImpl extends AbstractDao<Integer, UserProfile>impleme
 		crit.add(Restrictions.eq("type", type));
 		return (UserProfile) crit.uniqueResult();
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<UserProfile> findAll(){
+	public List<UserProfile> findAll() {
 		Criteria crit = createEntityCriteria();
+		crit.add(Restrictions.not(Restrictions.eq("type", "ADMIN")));
 		crit.addOrder(Order.asc("type"));
-		return (List<UserProfile>)crit.list();
+		return (List<UserProfile>) crit.list();
 	}
-	
+
 }
